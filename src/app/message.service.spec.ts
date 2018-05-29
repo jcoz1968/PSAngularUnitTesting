54a753c3567a670062c7867b5c64037fd39b5e3c
Jeffery Cosby <jcoz1968@hotmail.com>
2018-05-28 22:58:32 -0500

    tests for messaging service

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -4,10 +4,30 @@ describe('MessageService', () => {
   let service: MessageService;
 
   beforeEach(() => {
-    service = new MessageService();
+
   });
 
   it('should have no messages to start', ()=> {
+    service = new MessageService();
+
+    expect(service.messages.length).toBe(0);
+  });
+
+  it('should add a message when add is called', () => {
+    service = new MessageService();
+
+    service.add('meesage1');
+
+    expect(service.messages.length).toBe(1);
+  });
+
+
+  it('should remove all messages when clear is called', () => {
+    service = new MessageService();
+    service.add('meesage1');
+
+    service.clear();
+
     expect(service.messages.length).toBe(0);
   });
 
